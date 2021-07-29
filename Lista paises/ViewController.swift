f8f61f857bb6f5800d1948797e05a47dacd02084
Harry Lopez <jharrylopez@gmail.com>
2021-07-28 21:14:23 -0500

    Crear proyecto de lista paises

--- Lista paises/ViewController.swift
+++ Lista paises/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // MARK: - Variables
+    @IBOutlet weak var tableview: UITableView!
+    
+    let paises = ["Colombia", "Perú", "Brasil", "Ecuador", "Bolivia", "Estados Unidos"] // Array String
+    
+    // MARK: - Metodos Ciclo de Vida
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableview.delegate = self
+        tableview.dataSource = self
+        
     }
 
+    // MARK: - Funciones delegados
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return paises.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = paises[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    // MARK: - Funciones
+    
+
+    
 
 }
 
